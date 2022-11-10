9d9decd499fb1d608a14026b1acc6c7466263389
Davin <davin-dj@hotmail.com>
2022-11-09 19:08:57 -0500

    Fixed typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const PORT = proceass.env.PORT
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
